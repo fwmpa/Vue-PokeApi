9b4fdea3cf7fcafa7240f4f5279e3dde50b8d50a
Fernando Andrade <fernandoandrade@Fernandos-MacBook-Pro.local>
2022-04-06 21:11:37 -0300

    Build simple Vue application and integrated API using Axios
    Signed-off-by: Fernando Andrade <fernandoandrade@Fernandos-MacBook-Pro.local>

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>
+    Requisições HTTP com Axios
+  </h1>
+  <div v-for="(pokemon, i) in pokemons" :key="i">
+    <p>{{ pokemon.name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, onMounted, ref } from "vue";
+  import api from "@/services/api";
+
+  export default defineComponent({
+    setup() { 
+      const pokemons = ref([]);
+
+      const fetchPokemons = () => api.get("/pokemon?limit=20").then((response) => pokemons.value = response.data.results);
+
+      onMounted(fetchPokemons);
+
+      return { pokemons }
+    }
+  });
+</script>
